fce08d2ec1010607bc1c4886b29a153d7d3bf9f7
Hidayat ali beigh <96649174+hidayat11111@users.noreply.github.com>
2022-09-21 11:53:24 +0530

    Update and rename example14.sh to Ex14.sh

--- Linux Internals/Bash/Ex14.sh
+++ Linux Internals/Bash/Ex14.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+:'programe to check substring' 
+
+
+
 echo "Enter String"
 read str
 
-echo "${str:0:2}" #subString one Way
+echo "${str:0:2}" #this is one way
 
 subStr=${str:2:5}
 echo "$subStr"
