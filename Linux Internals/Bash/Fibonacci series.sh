efe955c3e059703738bd46874b2d79d24f8de7de
Hidayat ali beigh <96649174+hidayat11111@users.noreply.github.com>
2022-09-21 20:08:35 +0530

    Create Fibonacci series.sh

---
+++ Linux Internals/Bash/Fibonacci series.sh
@@ -0,0 +1,21 @@
+# Program for Fibonacci
+# Series
+
+N=6
+a=0
+b=1
+
+echo "The Fibonacci series is : "
+
+for (( i=0; i<N; i++ ))
+do
+   echo -n "$a "
+
+    fn=$((a + b))
+     a=$b
+     b=$fn
+done
+
+
+
+
